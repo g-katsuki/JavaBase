2ff1a8e6574024f7f28883fe0b594ea2a9f08d99
Katsuki Go <go@KatsukinoMacBook-Air.local>
2024-07-28 21:28:07 +0900

    [WORKING] modified health controller

--- src/main/java/com/example/base/controller/HealthController.java
+++ src/main/java/com/example/base/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.base.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,7 @@ public class HealthController {
 
     @GetMapping("/health")
     public String healthCheck() {
+        System.out.println("health check is called");
         return "OK";
     }
 }
